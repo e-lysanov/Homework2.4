8127cab1402fa06b7067bcc393e97e7fa1c907e3
e-lysanov <e.lysanov@yandex.ru>
2023-04-19 22:49:50 +0300

    1st commit

---
+++ src/test/java/ru/lysanov/egor/homework2_4/Homework24ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lysanov.egor.homework2_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
